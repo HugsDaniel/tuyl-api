0ef8e6ec82519eb01c84dc7ef4be2a8c48e9285b
Hugo Daniel <hugodaniel.icam@gmail.com>
2019-06-17 01:54:39 +0200

    Render user skills as json

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,12 +2,15 @@ class Api::V1::UsersController < ApplicationController
   def dashboard
     @user = current_user
     user_activities = @user.user_activities.order('updated_at DESC')
+    user_skills = @user.user_skills
 
     @json_user_activites = user_activities.as_json(include: :activity)
+    @json_user_skills = user_skills.as_json(include: :skill)
 
     render json: {
       user_activities: @json_user_activites,
-      user: @user
+      user: @user,
+      user_skills: @json_user_skills
     }
   end
 end
