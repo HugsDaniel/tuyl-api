9d596724425cb9e6a72ab8749975bc3102041c52
Hugo Daniel <hugodaniel.icam@gmail.com>
2019-06-20 17:03:26 +0200

    Render correct format for user activity rendering

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,14 +1,25 @@
 class Api::V1::UsersController < ApplicationController
   def dashboard
     @user = current_user
-    user_activities = @user.user_activities.order('updated_at DESC')
+    user_activities = @user.user_activities.order('updated_at DESC').includes(:activity)
     user_skills = @user.user_skills
 
-    @json_user_activites = user_activities.as_json(include: :activity)
+    # @json_user_activites = user_activities.as_json(include: :activity)
+    @json_user_activities = user_activities.map do |ua|
+      {
+        id:       ua.id,
+        title:    ua.activity.name,
+        content:  ua.activity.description,
+        start:    ua.begin_time.strftime("%Y-%m-%d %H:%M"),
+        end:      ua.end_time.strftime("%Y-%m-%d %H:%M"),
+        status:   ua.status
+      }
+    end
+
     @json_user_skills = user_skills.as_json(include: :skill)
 
     render json: {
-      user_activities: @json_user_activites,
+      user_activities: @json_user_activities,
       user: @user,
       user_skills: @json_user_skills
     }
